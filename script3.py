dcb96fe3b37a96006949c6b62c68a56f67277fb1
Himanshu9714 <h9714348433@gmail.com>
2022-01-13 09:20:35 +0530

    Ignoring Promotions and others are working fine

--- script3.py
+++ script3.py
@@ -4,16 +4,22 @@ from datetime import datetime
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-game = []
+events = []
 game_dct = {}
 out = []
 buff = []
 is_duplicate_sport_str = None
 new_lst = []
 
+def add_game(participants, winning_selections, inner_game_dct, game):
+    inner_game_dct['pariticipants'] = participants
+    inner_game_dct['winning_selection'] = winning_selections
+    game.append(inner_game_dct)
+    return game
+
 with pdfplumber.open('events-FREVNOUT-11162021-A7A5ACECC8AB1DB56E14BD0232ED186F.PDF') as f:
     extracted = ''
-    for page_no in range(4):
+    for page_no in range(304,313):
         fpage = f.pages[page_no]
         extracted = extracted + fpage.extract_text()
     
@@ -46,32 +52,49 @@ with pdfplumber.open('events-FREVNOUT-11162021-A7A5ACECC8AB1DB56E14BD0232ED186F.
             if lst != None:
                 lst.append(l)
     new_lst.append(lst)
-    pp.pprint(new_lst)
+    out.clear()
+    # pp.pprint(new_lst)
 
     participant_rounds = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', 'Final/OT']
 
+    game_dct = {}
+    game = []
+    inner_game_dct = {}
+    participants = []
+    participants_dct = {}
+    winning_selections = []
+    winning_selections_dct = {}
+    winning_selections_flag = False
     for lst in new_lst:
-        game_dct = {}
-        participants = []
-        participants_dct = {}
-        winning_selections = []
-        winning_selections_dct = {}
-        winning_selections_flag = False
         for line in lst:
+            print(f"\nThis is line:{line}")
             
             if 'SPORT: ' in line:
+                if participants != [] and winning_selections != []:
+                    add_game(participants, winning_selections, inner_game_dct, game)
+
+                    participants = []
+                    winning_selections = []
+                    game_dct = {}
+                    game = []
+                if "Promotions" in line: break
                 game_dct['name'] = line
 
-            elif re.match(r'([a-zA-Z]|\s|/|\w)+[@]([a-zA-Z]|\s|/|\w+)+\s\d{2}/\d{2}/\d{4}', line):
-                game_dct['date'] = line[-37:-15]
-                game_dct['id'] = line[-9:-1]
+            elif re.match(r'([a-zA-Z]|\s|[\,\/\+\-\&\.\:]|\w)+[@]([a-zA-Z]|\s|[\,\/\+\-\&\.\:\?]|\w)+\(?([a-zA-Z\s]|\-|\d)*\)?\s\d{2}/\d{2}/\d{4}', line):
+                if participants != [] and winning_selections != []:
+                    add_game(participants, winning_selections, inner_game_dct, game)
+
+                    participants = []
+                    winning_selections = []
+                inner_game_dct = {}
+                inner_game_dct['date'] = line[-37:-15]
+                inner_game_dct['id'] = line[-9:-1]
 
             elif (("Away" in line) or ("Home" in line)) and (re.search('\(\d+\.\d\)',line)==None):
-                print(f"\n\nThis is line inside participant: {line}")
                 winning_selections_flag = False
                 participants_dct = {}
                 pattern = "(?:\d{1}|----)"
-                BI = re.search(r"(\d{4})", line)
+                BI = re.search(r"(\d{2,6})", line)
                 if BI:
                     participants_dct['BI'] = BI[0]
                 else:
@@ -82,19 +105,19 @@ with pdfplumber.open('events-FREVNOUT-11162021-A7A5ACECC8AB1DB56E14BD0232ED186F.
                     participants_dct['participant'] = ''
                 else:
                     participants_dct['participant'] = line[len(participants_dct['BI']):line.index(participants_dct['HA'])-1]
-                print(f"\n\n\nParticipant Dct: {len(participants_dct['BI'])}\nParticipant HA index: {line.index(participants_dct['HA'])}")
                 values = re.findall(pattern, line[4:])
 
                 for indx in range(len(participant_rounds)):
                     participants_dct[participant_rounds[indx]] = values[indx]
                 if len(participants_dct) == 13:
-                    participants.append(participants_dct)
+                    if not participants_dct in participants:
+                        participants.append(participants_dct)
+                    else: continue
         
             elif "Market Name" in line:
                 winning_selections_flag = True
             
             elif winning_selections_flag:
-                print(f"\n\nThis is line inside winning: {line}")
                 try:
                     check_continued_new_line = re.search('\(\d+\.\d\)', line)
                     BIP = line.startswith('BIParticipant')
@@ -138,10 +161,17 @@ with pdfplumber.open('events-FREVNOUT-11162021-A7A5ACECC8AB1DB56E14BD0232ED186F.
                         winning_selections_dct['winning_selection'] = '-----'
                 if winning_selections_dct in winning_selections:continue
                 else:winning_selections.append(winning_selections_dct)
+                print(f"Winning Selection: {winning_selections}")
 
-        game_dct['pariticipants'] = participants
-        game_dct['winning_selection'] = winning_selections
-        game.append(game_dct)
+        game_dct['game'] = game
+        if not game_dct in events:
+            events.append(game_dct)
+    if participants != [] and winning_selections != []:
+        add_game(participants, winning_selections, inner_game_dct, game)
+    print(f"Game dict outside for loop {game_dct}")
+    if game_dct!={} and game_dct not in events:
+        print("\n\n\nLast if called\n\n")
+        events.append(game_dct)
        
 print("\n\n\n\n\n\*********************\n\n\n\n\*************\n\nResult******\n\n\n")
-pp.pprint(game)
+pp.pprint(events)
